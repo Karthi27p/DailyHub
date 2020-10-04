6f503c4d1cf181748b5523a937bf6800f3209774
Karthi <karthi.palaniappan@imaginea.com>
2020-10-04 14:59:04 +0530

    Handled keyboard avoiding view for ios 14

--- Atoms/DHKeyboardAdaptive.swift
+++ Atoms/DHKeyboardAdaptive.swift
@@ -54,16 +54,16 @@ extension UIResponder {
 }
 
 struct KeyboardAdaptive: ViewModifier {
-    @State private var bottomPadding: CGFloat = 0
+    @State private var topPadding: CGFloat = 0
     func body(content: Content) -> some View {
         GeometryReader{ geometry in
-            content.padding(.bottom, self.bottomPadding).onReceive(Publishers.keyboardHeight) { keyboardHeight in
+            content.padding(.top, self.topPadding).onReceive(Publishers.keyboardHeight) { keyboardHeight in
                    
                     let keyboardTop = geometry.frame(in: .global).height - keyboardHeight
                     
-                let focusedTextInputBottom = UIResponder.currentFirstResponder?.globalFrame.maxY ?? 0
+                let focusedTextInputBottom = UIResponder.currentFirstResponder?.globalFrame.minY ?? 0
                     
-                self.bottomPadding = max(0, focusedTextInputBottom - keyboardTop - geometry.safeAreaInsets.bottom) 
+                self.topPadding = -max(0, focusedTextInputBottom - 10 - keyboardTop - geometry.safeAreaInsets.top)
             }
             
             .animation(.easeOut(duration: 0.16))
